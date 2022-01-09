fd9da3e7e6698b07d3e34e74e3d2b9c33d16420a
@jrivera <jrivera@expertsystems.pe>
2022-01-09 05:14:31 -0500

    agregadoTablas

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/admin', function () {
+    return view('layouts.admin');
+});
+
+Route::get('/index', function () {
+    return view('almacen.index');
+});
+
+Route::get('/principal', function () {
+    return view('layouts.principal');
 });
 
 Auth::routes();
